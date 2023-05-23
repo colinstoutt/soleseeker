bcee6564c156d9b1f50c15bb77446259703b577c
cps97 <colinstoutt@gmail.com>
2023-05-23 15:47:13 -0700

    make layout responsive

--- src/components/SmallCarousel.jsx
+++ src/components/SmallCarousel.jsx
@@ -20,8 +20,25 @@ export const SmallCarousel = ({ images, shoe }) => {
   return (
     <>
       {images ? (
-        <>
+        <div className="lg:w-[500px]">
           <div className="relative flex items-center justify-center mt-10 mb-10">
+            <svg
+              onClick={() => handleDecrement()}
+              className="w-8 h-8 cursor-pointer bg-white shadow-sm p-2 no-select"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              ></path>
+            </svg>
+
             {images.map((image, index) =>
               index === carouselIndex ? (
                 <div
@@ -29,13 +46,30 @@ export const SmallCarousel = ({ images, shoe }) => {
                   className="relative bg-white flex justify-center items-center h-60"
                 >
                   <img
-                    className="p-10 w-[350px] h-auto"
+                    className="p-10 w-[350px] lg:w-[450px] h-auto"
                     src={image}
                     alt={shoe.name}
                   />
                 </div>
               ) : null
             )}
+
+            <svg
+              onClick={() => handleIncrement()}
+              className="w-8 h-8 cursor-pointer bg-white shadow-sm p-2 no-select"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              ></path>
+            </svg>
           </div>
           <div className="flex items-center justify-center gap-3 mb-10">
             {images.map((image, index) => (
@@ -48,7 +82,7 @@ export const SmallCarousel = ({ images, shoe }) => {
               />
             ))}
           </div>
-        </>
+        </div>
       ) : null}
     </>
   );
